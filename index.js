98401c50643258d7de598c8d717a98d7a5d639ed
2kw <type2kw@gmail.com>
2021-01-01 23:20:03 +0900

    기본 function

--- index.js
+++ index.js
@@ -10,4 +10,13 @@ const sayHi2 = (name, age, gender) => {
     console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
 };
 sayHi2(name, age); // -> Hello John, you are 24, you are a undefined = {gender?}를 통해 옵셔널 값으로 타입 지정 가능 
+// argument에 대한 타입지정, function의 return 타입 지정
+const sayHi3 = (name, age, gender) => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+sayHi3("Henry", 26, "male");
+const sayHi4 = (name, age, gender) => {
+    return `Hello ${name}, you are ${age}, you are a ${gender}`;
+};
+console.log(sayHi4("Henry", 26, "male"));
 //# sourceMappingURL=index.js.map
