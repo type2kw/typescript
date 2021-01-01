5ee164ec205c91444d8c91f603006d045c027197
2kw <type2kw@gmail.com>
2021-01-01 22:45:43 +0900

    기본 function

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-console.log("hello");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "John", age = 24, gender = "Male";
+const sayHi = (name, age, gender) => {
+    console.log('Hello ${name}, you are ${age}, you are a ${gender}');
+};
+sayHi(name, age, gender);
 //# sourceMappingURL=index.js.map
