c948aa65847a3aa269019fd51c97dd2118b107b9
2kw <type2kw@gmail.com>
2021-01-01 22:45:03 +0900

    기본 function

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log("hello");
+const name = "John",
+    age = 24,
+    gender = "Male";
+
+const sayHi = (name, age, gender) => {
+    console.log('Hello ${name}, you are ${age}, you are a ${gender}');
+}
+
+sayHi(name, age, gender);
+
+export {};
