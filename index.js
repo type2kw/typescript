6fcff310e60cefdef38142dd3ff76bc5f891c5f0
2kw <type2kw@gmail.com>
2021-01-01 23:05:42 +0900

    기본 function

--- index.js
+++ index.js
@@ -4,5 +4,10 @@ const name = "John", age = 24, gender = "Male";
 const sayHi = (name, age, gender) => {
     console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
 };
-sayHi(name, age, gender);
+sayHi(name, age, gender); // -> Hello John, you are 24, you are a Male
+// sayHi(name, age); -> error TS2554: Expected 3 arguments, but got 2. 에러발생 = 컴파일시 타입체크를 통해 오류를 잡아줌
+const sayHi2 = (name, age, gender) => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+sayHi2(name, age); // -> Hello John, you are 24, you are a undefined = {gender?}를 통해 옵셔널 값으로 타입 지정 가능 
 //# sourceMappingURL=index.js.map
